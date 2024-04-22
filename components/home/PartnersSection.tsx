6cb2e86a15f66ac68e897ff2fff7368ad798a3b5
devabdth <a.tharwat@cubersio.com>
2024-04-22 14:22:11 +0200

    Abandon Partners Section from the Draft

--- components/home/PartnersSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PartnersSection= () => {
-    return (
-        <section className="text-primary">Parnters Section</section>
-    )
-}
-
-export default PartnersSection
