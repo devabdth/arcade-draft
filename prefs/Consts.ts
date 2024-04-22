0f539d0ca8f22439ec573c031fb473e98e8bf8f3
devabdth <a.tharwat@cubersio.com>
2024-04-22 05:16:23 +0200

    Consts Init

---
+++ prefs/Consts.ts
@@ -0,0 +1,30 @@
+export const NavTabs= [
+    {
+        label: "Home",
+        href: "/",
+    },
+    {
+        label: "About Us",
+        href: "/about/",
+    },
+    {
+        label: "Shop",
+        href: "/shop/",
+    },
+    {
+        label: "Programs",
+        href: "/programs/",
+    },
+    {
+        label: "Partners",
+        href: "/partners/",
+    },
+    {
+        label: "Careers",
+        href: "/careers/",
+    },
+    {
+        label: "Family",
+        href: "/family/",
+    },
+];
