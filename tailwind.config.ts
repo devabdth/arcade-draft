ca07ced6e8715b975ed05cc8a85c64a2b56a7ad2
devabdth <a.tharwat@cubersio.com>
2024-04-22 05:54:24 +0200

    Config Project Colors & Fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        fontFamily: {
+            primary: "Robotaur",
+            secondary: "Sukplena Roman",
+        },
+        colors: {
+            background: "#111111",
+            primary: "#f9f9f9",
+            secondary: "#929292" 
+        },
     },
   },
   plugins: [],
