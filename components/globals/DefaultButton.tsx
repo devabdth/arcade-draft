d3cd9785e8194f305e263ddf1b621d726deceebd
devabdth <a.tharwat@cubersio.com>
2024-04-22 02:28:51 +0200

    Default Button

---
+++ components/globals/DefaultButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type DefaultButtonProps= {
+    title: string;
+    variant: string;
+    onClick: ()=> {};
+}
+export default (props: DefaultButtonProps) => {
+    return (
+        <button
+        className={props.variant === "primary" ? 
+        "px-4 py-2 bg-primary text-black font-secondary font-regular rounded-3xl transition-all hover:bg-transparent hover:text-primary border-2 border-primary"
+        :"px-4 py-2 text-primary font-secondary font-regular rounded-3xl border-2 border-primary"}
+        onClick={props.onClick}
+        >{props.title}</button>
+    );
+}
